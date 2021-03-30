d2ae2a68d4fdf16753ea2c51cf0e892ab03bd826
Connie Ho <connie3@ualberta.ca>
2021-03-30 23:09:44 +0000

    fix new category bug with outer join

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,7 +2,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   
   def index
-    @categories = Category.joins(:products).select('categories.*, COUNT(products.id) as product_count').group('categories.id')
+    @categories = Category.joins('LEFT OUTER JOIN products on products.category_id = categories.id').select('categories.*, COUNT(products.id) as product_count').group('categories.id').order(created_at: :desc)
   end
 
   def new
@@ -10,7 +10,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Product.new(category_params)
+    @category = Category.new(category_params)
 
     if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
