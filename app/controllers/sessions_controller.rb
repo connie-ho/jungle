34c2c85b9b9484fc869cdcb405554c8db42cbec9
Connie Ho <connie3@ualberta.ca>
2021-03-31 16:05:39 +0000

    style login form and fix session to allow for email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
-    @user = User.new
   end
 
   def create
     @user = User.find_by(email: params[:email])
-    puts @user.inspect
-    puts "HELLLOOO"
 
     # If the user exists AND the password entered is correct.
     if @user && @user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = @user.id
+      session[:email] = @user.email
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
