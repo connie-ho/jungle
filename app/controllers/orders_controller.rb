1359c44d1234ebcc15c3e47e3a1f5e9cd884e347
Connie Ho <connie3@ualberta.ca>
2021-05-03 21:03:19 +0000

    remove duplicate order confirmation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,7 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
-      redirect_to order, notice: 'Your order has been placed.'
+      redirect_to order
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
