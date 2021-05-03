4b1dc72a1b616aff6f80cd1e6a19834ac91ddd04
Connie Ho <connie3@ualberta.ca>
2021-05-03 22:18:46 +0000

    send email after order is validated

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
+      OrderMailer.order_confirmation(order).deliver_now
       redirect_to order
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
