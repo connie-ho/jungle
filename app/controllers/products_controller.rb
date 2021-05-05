ec94eaad118e7745c43b4aca9ce7154e8f0906f0
Connie Ho <connie3@ualberta.ca>
2021-05-05 00:15:04 +0000

    update product show view to display form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
     @reviews = Review.joins('INNER JOIN users ON users.id = reviews.user_id').select('reviews.*, users.first_name AS user_first_name, users.last_name AS user_last_name').where(product_id: @product.id)
-    # raise @reviews.inspect
+    @review = Review.new
   end
 
 end
